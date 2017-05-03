6750c1345ee5a5d8fd0e077433cdca0121feb835
Carsten Witt <carsten.witt@gmail.com>
2017-05-03 10:38:13 +0200

    Improve description

--- Iccprofiles.rb
+++ Iccprofiles.rb
@@ -1,5 +1,5 @@
 class Iccprofiles < Formula
-	desc "ICC profiles for linear TIFF and film negative handling"
+	desc "ICC profiles for linear TIFF and digital film negative handling"
 	homepage "https://github.com/tomkyle/negatives-iccprofiles"
 	url "https://github.com/tomkyle/negatives-iccprofiles/archive/1.1.0.tar.gz"
 	sha256 "f54db7b32491eb09651de09a1af4bc29fcafca8259228eb4362d3dec8e5c93e3"
