ab10754e32e42fcfb0e955263ae22e1ef02a3907
Carsten Witt <carsten.witt@gmail.com>
2017-05-04 13:49:21 +0200

    Update to new version

--- Positive.rb
+++ Positive.rb
@@ -1,11 +1,8 @@
 class Positive < Formula
   desc "Convert film negative TIFFs to positives. Powered by GNU Parallel."
   homepage "https://github.com/tomkyle/negatives-positive"
-  url "https://github.com/tomkyle/negatives-positive/archive/1.0.1.tar.gz"
-  sha256 "5328dd948abd63479465d151da7822b3d240d7176ba4bff2ed6940189af940f3"
-
-  # Redundant since version taken from archive link above.
-  # version "1.0.1"
+  url "https://github.com/tomkyle/negatives-positive/archive/1.0.2.tar.gz"
+  sha256 "6a41f4f1ea9a85eba5f8699bb53c4807d46820c37f2a879b9d339e1f62b99ee3"
 
   bottle :unneeded
 
@@ -18,5 +15,12 @@ class Positive < Formula
     prefix.install "USAGE"
     prefix.install "CREDITS"
     prefix.install "LICENSE"
+  end
+
+  test do
+    # Check if "positive -a [OPTIONS]" is part of output
+    # when called without parameters.
+    # Expected exit code is 1.
+    assert_match "positive \-a \[OPTIONS\]", shell_output("#{bin}/positive", 1)
   end
 end
