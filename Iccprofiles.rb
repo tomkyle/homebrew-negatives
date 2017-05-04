4036ad921c59de041b9f43c7c24c7951ab1c7e11
Carsten Witt <carsten.witt@gmail.com>
2017-05-04 11:25:56 +0200

    Fix audit issue

--- Iccprofiles.rb
+++ Iccprofiles.rb
@@ -1,11 +1,8 @@
 class Iccprofiles < Formula
-	desc "ICC profiles for linear TIFF and digital film negative handling"
+	desc "ICC profiles for linear and digital film negative TIFF handling"
 	homepage "https://github.com/tomkyle/negatives-iccprofiles"
 	url "https://github.com/tomkyle/negatives-iccprofiles/archive/1.1.2.tar.gz"
 	sha256 "181287e811871eec276c0aaebc31f032c666337efd2aaf03b8eeb46411dc22cc"
-
-	# Redundant since version taken from archive link above.
-	# version "1.1.2"
 
 	bottle :unneeded
 
