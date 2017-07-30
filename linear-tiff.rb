64c27f98f47c691c9f8c7b39e3f2e259a9990ed8
Carsten Witt <carsten.witt@gmail.com>
2017-07-30 12:00:34 +0200

    Use new version

--- linear-tiff.rb
+++ linear-tiff.rb
@@ -1,12 +1,12 @@
 class LinearTiff < Formula
   desc "Convert Raw photos to Linear TIFF: dcraw combined with GNU Parallel."
   homepage "https://github.com/tomkyle/negatives-linear-tiff"
-  url "https://github.com/tomkyle/negatives-linear-tiff/archive/1.1.5.tar.gz"
-  sha256 "2f832a5179802cc675d60689786e12040cfdc7d26496bc488b06c3ec6ee22aa5"
+  url "https://github.com/tomkyle/negatives-linear-tiff/archive/1.1.6.tar.gz"
+  sha256 "381ab311ad6ac757fcbe07669663e97ef2915e57899915361031e581a99fd2a2"
 
   bottle :unneeded
 
-  depends_on "dcraw"
+  depends_on "libraw"
   depends_on "exiftool"
   depends_on "imagemagick"
   depends_on "parallel"
